5f9300d1bfb688661db2085ddb801c57e73dfcb0
Devynne Marshall <devynnem@gmail.com>
2022-12-11 10:09:11 -0700

    Initial commit

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,30 @@
-var foods = {apples: 23, 
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
+  eggs: 48
+};
+//curly bracket should be by itself on the first and last line. And added a semicolon at the end.
 
-var person = { height: "6 ft", weight: "160 lbs" }
+var person = { 
+  height: "6 ft", 
+  weight: "160 lbs" 
+};
+//Again, culy brackets should be by themselves. And key-values should be on their own line.
 
-var post = { imageSrc: "./images/beach.png",
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+};
+// Culy bracket should be on the first line with Object.
 
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
-}
+var post2 = {
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
+};
+//Deleted weird spacing to make it easier to read.
